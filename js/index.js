f2ba1e28c4bbc87a4a7bd854404803b62bba8187
Irina Brennen <irinabrennen@twc.com>
2017-10-02 00:42:21 -0500

    first commit for a basic (and completely unstyled) version of the project

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+var urlStreams = "https://wind-bow.glitch.me/twitch-api/streams/";
+var urlUsers = "https://wind-bow.glitch.me/twitch-api/users/";
+
+var frequentStreams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
+
+$(document).ready(function(){
+
+    var logoURL;
+    var channelURL;
+
+    frequentStreams.forEach( function(element, index) {
+        
+        var urlUser = urlUsers + element;
+        $.getJSON(urlUser, function(data) {
+
+            // if (data.stream != null){
+            
+            //     channelURL = data.stream.channel.url;
+            //     $("#twitch-res1-pic > img").css("visibility", "visible");
+            //     $("#twitch-res1-pic > img").attr("src", logoURL);       
+            //     $("#twitch-res1-name").html(data.stream.channel.display_name);
+            //     $("#twitch-res1-name").attr("href", channelURL);
+            //     $("#twitch-res1-status").html(data.stream.channel.status);
+            //
+
+            var display_name = data.display_name;
+            var imgHtmlElement = "#twitch-res" + (index + 1) + "-pic > img" ;
+            var nameDivHtlmElement = "#twitch-res" + (index + 1) + "-name";
+            var statusDivHtmlElement = "#twitch-res" + (index + 1 ) + "-status"
+
+            $(imgHtmlElement).attr("src", data.logo); 
+            $(nameDivHtlmElement).html(display_name);
+            $(nameDivHtlmElement).attr("href", "https://www.twitch.tv/" + element);
+
+            var urlStream = urlStreams + display_name;
+
+            $.getJSON(urlStream, function(data) {
+
+                if (data.stream == null ) 
+                    $(statusDivHtmlElement).html("Not Currently Streaming");
+                else
+                    $(statusDivHtmlElement).html(data.stream.channel.status);
+
+            });
+
+        });
+
+    }, this);      
+   
+}); 
